630f33c84354a69ba515dfaa30873af39dbdf212
sahil bishnoi <sahilbishnoi170@gmail.com>
2024-01-31 14:31:06 +0530

    post schema added

--- src/models/post.js
+++ src/models/post.js
@@ -23,16 +23,49 @@ const userSigninSchema = new mongoose.Schema({
 
 
 const PostSchema = new mongoose.Schema({
-    userid  : {
-        type: mongoose.Schema.Types.ObjectId,
-        ref : 'User',
-        required: true,
+    creator: {
+      type: ObjectId,
+      required: true,
+      ref: "User"
     },
-    post : {
-        postText : String,
-        img: { data: Buffer, contentType: String },
-    }
-})
+    content: {
+      type: "String",
+      required: true,
+    },
+    reactions: {
+      type: {
+        likes: {
+          type: Number,
+          default: 0
+        },
+        dislikes: {
+          type: Number,
+          default: 0
+        },
+        laughs: {
+          type: Number,
+          default: 0
+        },
+      }
+    },
+    comments: {
+      type: [ObjectId],
+      ref: "Comments"
+    },
+    media: {
+      type: {
+        image: {
+          type: "String", // URL or reference to image storage
+          optional: true
+        },
+        video: {
+          type: "String", // URL or reference to video storage
+          optional: true
+        },
+      }
+    },
+  }
+  )
 
 const commentSchema = new mongoose.Schema({
     postid : {
